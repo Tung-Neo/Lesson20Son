35527cc69133e3c80a2abc13563e8e0bd846cde3
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2020-04-03 15:02:29 +0700

    Update TestResizableCircle.java

--- TestResizableCircle.java
+++ TestResizableCircle.java
@@ -2,8 +2,7 @@ package Lesson20.Lesson20Son;
 
 public class TestResizableCircle {
     public static void main(String[] args) {
-        ResizableCircle r1 = new ResizableCircle(2);
-        r1.resize(2);
-        System.out.println("Area is " + r1.getArea());
+        ResizableCircle r1 = new ResizableCircle(3);
+        System.out.println("Area is " + r1.resize(30));
     }
 }
